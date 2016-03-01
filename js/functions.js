6285ff0a501a05cb3ac9013eb049e918aa6a9f02
Teh-Yak <Teh-Yak@users.noreply.github.com>
2016-03-01 15:41:42 +0000

    Update disclaimer

--- js/functions.js
+++ js/functions.js
@@ -71,9 +71,9 @@ function init(colours) {
     //calculator.setExpression({id: "pshade", latex: "g(x)<y<h(x)", color: "#00ff00"});
 }
 $(document).ready(function(){
-	alert("This webpage can use cookies to store information on the preferences of the graph. \n "+
-	"This is highly experimental, subject to change but will not impact the main functioning of the page. \n" +
-	"If you do not wish to use cookies do not click the button which is labelled 'Enable cookies'. \n"
+	alert("This webpage can use cookies to store information on the preferences of the graph. "+
+	"This is highly experimental, subject to change but will not impact the main functioning of the page." +
+	"If you do not wish to use cookies do not click the button which is labelled 'Enable cookies'."
 	"Thank you for your patience."
 	);
 	
