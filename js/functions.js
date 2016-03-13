801e94fdb7305be598fa150f1a2858ca976f0e9e
CaughtEx <harryjshipton@gmail.com>
2016-03-13 12:52:00 +0000

    Finished patches for the graph
    It's now possible to have as many variable costs as you want now.

--- js/functions.js
+++ js/functions.js
@@ -39,12 +39,12 @@ function init() {
     var total = 0;
     var str = "g(x)=f+v_0x";
     for(var j=0; j < all.length; j++) {
-        calculator.setExpression({id: "v"+j, latex: "v_"+j+"="+all[j][0]});
+        calculator.setExpression({id: "v"+j, latex: "v_{"+j+"}="+all[j][0]});
         if(j!=0) {
             total += all[j-1][0]*all[j][1];
             total -= all[j][0]*all[j][1];
             calculator.setExpression({id: "p"+j, latex: "p_{"+j+"}="+all[j][1]});
-            str += "+(1/2)(v_"+j+"-v_"+(j-1)+")(x+abs(x-p_{"+j+"})-p_{"+j+"})";
+            str += "+(1/2)(v_{"+j+"}-v_{"+(j-1)+"})(x+abs(x-p_{"+j+"})-p_{"+j+"})";
         }
         /*
         else {
